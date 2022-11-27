18530e346455ce330a145d808213032e7ee748ea
Iurii Galchenko <galchenkou@gmail.com>
2022-11-27 10:01:00 +0100

    changed create_posts migration

--- database/migrations/2022_11_24_204000_create_posts_table.php
+++ database/migrations/2022_11_24_204000_create_posts_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 255);
+            $table->text('text');
+            $table->unsignedInteger('views');
             $table->timestamps();
         });
     }
