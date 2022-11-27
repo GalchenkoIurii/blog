c3a4f2f66a36d018a46d50c931bc0822c30dbf33
Iurii Galchenko <galchenkou@gmail.com>
2022-11-27 20:10:19 +0100

    added foreign key constraints to posts table

--- database/migrations/2022_11_24_204000_create_posts_table.php
+++ database/migrations/2022_11_24_204000_create_posts_table.php
@@ -15,8 +15,11 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->string('title', 255);
+            $table->foreignId('user_id')
+                ->nullable()
+                ->constrained()
+                ->nullOnDelete();
+            $table->string('title');
             $table->text('content');
             $table->string('img')->nullable();
             $table->unsignedBigInteger('views');
