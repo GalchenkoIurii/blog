3edf10180e1200b61c492d685206a04e4fc73655
Iurii Galchenko <galchenkou@gmail.com>
2022-11-28 22:54:57 +0100

    changed posts migration set default value for views, refactored js for modal window, added title field to create posts page and refactored posts.index route name

--- resources/js/ui.js
+++ resources/js/ui.js
@@ -1,5 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector('button.close-btn').addEventListener('click', () => {
-        document.querySelector('.modal').style.display = 'none';
-    });
+    const closeBtn = document.querySelector('button.close-btn'),
+        modal = document.querySelector('.modal');
+
+    if (closeBtn) {
+        closeBtn.addEventListener('click', () => {
+            modal.style.display = 'none';
+        });
+    }
+
 });
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -8,10 +8,18 @@
     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
         <form method="POST" action="{{ route('posts.store') }}">
             @csrf
+            <div class="mb-4">
+                <label for="price" class="block text-sm font-medium text-gray-700">Post title</label>
+                <div class="relative mt-1 rounded-md shadow-sm">
+                    <input type="text" name="title" id="title" class="block w-full rounded-md border-gray-300 pl-4 pr-4
+                    focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="Title">
+                </div>
+            </div>
             <textarea
                 name="content"
                 placeholder="{{ __('Text of the post') }}"
-                class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200
+                focus:ring-opacity-50 rounded-md shadow-sm"
             >{{ old('text') }}</textarea>
             <x-input-error :messages="$errors->get('text')" class="mt-2" />
             <x-primary-button class="mt-4">{{ __('Publish') }}</x-primary-button>
